0e3b36304b5c1afe04ce20d6ff4cffbd243d024a
Kyohei <kalops22.kikoo@gmail.com>
2019-09-04 16:47:45 +0200

    Ajout des champs Crimes et lieu

--- InspecteurSociete/Assets/Scripts/Jeu/AffichageCarteSuspect.cs
+++ InspecteurSociete/Assets/Scripts/Jeu/AffichageCarteSuspect.cs
@@ -8,7 +8,9 @@ public class AffichageCarteSuspect : MonoBehaviour
 {
     public GameMainScript mainScript;
     public TextMeshProUGUI victime;
+    public TextMeshProUGUI crimeCommis;
     public TextMeshProUGUI suspect;
+    public TextMeshProUGUI lieu;
     public TextMeshProUGUI description;
     public Image image;
 
@@ -18,11 +20,15 @@ public class AffichageCarteSuspect : MonoBehaviour
     {
         victime.text = carte.victime;
 
+        crimeCommis.text = carte.crimeCommis;
+
         suspect.text = string.Empty;
         foreach (var nomSuspect in carte.ListeSuspects)
         {
             suspect.text += nomSuspect + "\n";
         }
+
+        lieu.text = carte.lieu;
 
         description.text = carte.descriptionCrime;
         image.sprite = carte.sprite;
