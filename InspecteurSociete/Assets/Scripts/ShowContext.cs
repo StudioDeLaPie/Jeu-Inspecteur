e8fb90f1a521d7bf02b5e06945beeea31dc69006
Kyohei <kalops22.kikoo@gmail.com>
2019-09-05 11:04:33 +0200

    Ajout du bouton context dans la boucle de jeu

---
+++ InspecteurSociete/Assets/Scripts/ShowContext.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class ShowContext : MonoBehaviour
+{
+    public GameObject contextMesh;
+
+    public void ShowContextView()
+    {
+        contextMesh.SetActive(true);
+    }
+
+    public void HideContextView()
+    {
+        contextMesh.SetActive(false);
+    }
+}
